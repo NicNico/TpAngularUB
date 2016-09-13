08e2a71019f182253471c83c5637f103ce40a744
Alumno <Alumno>
2016-09-12 22:16:02 -0300

    Ejercico Facturacion

--- jsFerreteFacturacion.js
+++ jsFerreteFacturacion.js
@@ -3,12 +3,26 @@ var app= angular.module("FerreteFacturacion",[]);
 app.controller("controlFacturacion",function($scope){
 
 		$scope.datos={};
+        $scope.mensajeRespuesta="sin rta";
+		//$scope.datos.precioUno="111";
 
-		$scope.datos.precioUno="222";
-
-		$scope.sumar=function(){
-
-			alert("sumando");
+		$scope.sumar=function(dato1, dato2, dato3){
+		$scope.mensajeRespuesta=(parseInt(dato1)+parseInt(dato2)+parseInt(dato3));
+        alert($scope.mensajeRespuesta);
 		};
+
+
+		//$scope.datos.precioDos="222";
+		$scope.promedio=function(){
+
+			alert("promedio");
+		};		
+
+
+		//$scope.datos.PrecioTres="333";
+		$scope.precioFinal=function(){
+
+			alert("precioFinal");
+		};				
 
 });
